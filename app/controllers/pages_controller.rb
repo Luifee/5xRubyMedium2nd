d2d968a6bb6edf62b49bb5d0520488308dca5ce2
Luifee <lynnyuhuang@gmail.com>
2021-04-14 21:57:07 +0800

    1.15

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
   def index
+    @memos = Memo.order(created_at: :desc).includes(:user).take(6)
+  end
+
+  def show
+    @memo = Memo
+  end
+
+  def user
+    @user = User
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations' }
 
+  get '@:username/:memo_id', to: 'pages#show', as: 'memo_page'
+  get '@:username', to: 'pages#user', as: 'user_page'
+
   resources :memos
   root 'pages#index'
 end
